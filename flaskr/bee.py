645f0e8bc58e084818be732a1acb31922959de3d
Alexander Baker <bakera5@flip2.engr.oregonstate.edu>
2022-02-16 14:02:18 -0800

    Added "real" rows to bee types and tasks, added two new table pages for M:M

--- flaskr/bee.py
+++ flaskr/bee.py
@@ -49,33 +49,21 @@ def get_bee(bee_id, check_author=True):
 @bp.route('/<int:bee_id>/updateBee', methods=('GET', 'POST'))
 def update(bee_id):
     bee = get_bee(bee_id)
+    db_connection = connect_to_database()
 
     if request.method == 'POST':
         bee_type = request.form['bee_type']
         dob = request.form['dob']
         bee_name = request.form['bee_name']
-        error = None
 
-        if not bee_type:
-            error = 'bee_type is required'
-        
-        if not dob:
-            error += ' dob is required'
+        query = 'UPDATE Bees SET bee_type = %s, dob = %s, bee_name = %s WHERE bee_id = %s;'
+        data = (bee_type, dob, bee_name, bee_id)
+        execute_query(db_connection, query, data)
+        return redirect(url_for('bee.index'))
 
-        if not bee_name:
-            error += ' bee_name is required'
-
-        if error is not None:
-            flash(error)
-
-        else:
-            db_connection = connect_to_database()
-            query = 'UPDATE Bees SET bee_type = %s, dob = %s, bee_name = %s WHERE bee_id = %s;'
-            data = (bee_type, dob, bee_name, bee_id)
-            execute_query(db_connection, query, data)
-            return redirect(url_for('bee.index'))
-
-    return render_template('bee/updateBee.html', bee=bee)
+    query = 'Select type_id, type_name FROM Bee_Types ORDER BY type_id ASC;'
+    types = execute_query(db_connection, query).fetchall();
+    return render_template('bee/updateBee.html', bee=bee, types=types)
 
 @bp.route('/<int:bee_id>/deleteBee', methods=('POST',))
 def delete(bee_id):
